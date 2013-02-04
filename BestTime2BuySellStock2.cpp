20d53e29d0742c541091b78b95d5fe66606674aa
wenzhenwei <wenzhenwei@wenzw-OptiPlex-990.localdomain>
2013-02-04 21:52:19 +0800

    create
    Signed-off-by: wenzhenwei <wenzhenwei@wenzw-OptiPlex-990.localdomain>

---
+++ BestTime2BuySellStock2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int maxprofile = 0;
+        int min = 0x7FFFFFFF;
+        int max = 0;
+        vector<int>::iterator it = prices.begin();
+        for (; it != prices.end(); it++)
+        {
+            int cur = *it;
+            if (cur < min)
+            {
+                min = cur;
+                max = cur;
+            }
+            if (cur > max)
+            {
+                max = cur;
+                int p = max - min;
+                if (p > 0) maxprofile += p;
+                min = cur;
+            }
+        }
+        return maxprofile;
+    }
+};
